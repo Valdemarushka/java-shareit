705b619cb727d1a707e06d7bd955b45a0cd6654b
paradizer <Valdemarushka89@yandex.ru>
2022-12-07 20:15:39 +0700

    ТЗ-16. попытка номер три. Fix handler

--- server/src/main/java/ru/practicum/shareit/exeption/ExeptionHandler.java
+++ server/src/main/java/ru/practicum/shareit/exeption/ExeptionHandler.java
@@ -1,14 +1,12 @@
 package ru.practicum.shareit.exeption;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -52,26 +50,6 @@ public class ExeptionHandler {
     public ResponseEntity<String> handleWrongOwner(WrongOwnerExeption e) {
         log.error("Ошибка: неверный владелец");
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<Map<String, String>> handleConstraintViolation(ConstraintViolationException e) {
-        log.error("Ошибка: нарушение ограничения целостности базы данных");
-        Map<String, String> map = new HashMap<>();
-        e.getConstraintViolations().forEach(error -> {
-            String message = error.getMessage();
-            map.put("error", message);
-        });
-        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<Map<String, String>> handleConversionFailed(ConversionFailedException e) {
-        log.error("Ошибка: несуществующий параметр");
-        Map<String, String> map = new HashMap<>();
-        String message = "Unknown state: " + e.getValue();
-        map.put("error", message);
-        return new ResponseEntity<>(map, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler
